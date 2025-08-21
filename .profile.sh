df3451f43ecaf553cb08ed32ecb5d2732a12b6fd
kevinfiol <me@kevinfiol.com>
2025-08-21 17:28:34 -0400

    update profile

--- .profile.sh
+++ .profile.sh
@@ -19,11 +19,18 @@ alias apt:update="sudo apt update && sudo apt autoclean && sudo apt clean && sud
 alias rmm='rm -rf'
 alias hosts='cat $HOME/.ssh/config'
 alias pw="pwgen -c -n -y -s -B 16 1 | tr -d '\n' | tee >(wl-copy) && echo"
+
+# services
 alias plex:start="sudo systemctl start plexmediaserver.service"
 alias plex:stop="sudo systemctl stop plexmediaserver.service"
 alias plex:status="sudo systemctl status plexmediaserver.service"
 alias sunshine:start="systemctl --user start sunshine"
 alias sunshine:stop="systemctl --user stop sunshine"
+alias keyd:start="sudo systemctl start keyd"
+alias keyd:stop="sudo systemctl stop keyd"
+alias keyd:monitor="sudo keyd monitor"
+alias keyd:reload="sudo keyd reload"
+alias keyd:edit="sudo micro /etc/keyd/default.conf"
 
 ## git aliases
 alias push='git push origin $(git branch --show-current)'
